9bc90b3814c4388c9a753b69687633672b3085ed
ayim <ayim.shaimbetova@alatoo.edu.kg>
2023-12-21 11:21:47 +0600

    start pro

--- blog/ABlog/ABlog/settings.py
+++ blog/ABlog/ABlog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-th!7gu+m-2ia$_26r_8n0279vc6z6v_27pu@pi(91cmg0iefn9'
+SECRET_KEY = 'django-insecure-t(6v+5l2%5tareyytus8qihdols9$=j@23)&5dsxks9w__#bse'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "blog.apps.BlogConfig",
 ]
 
 MIDDLEWARE = [

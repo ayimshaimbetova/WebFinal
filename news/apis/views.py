cd9c081bcaf6caa4784a296bdda16fec4fc7e3b9
ayim <ayim.shaimbetova@alatoo.edu.kg>
2023-12-24 00:00:17 +0600

    create with api

--- news/apis/views.py
+++ news/apis/views.py
@@ -3,6 +3,20 @@ from rest_framework import generics, permissions
 from articles.models import Article
 from .serializers import ArticleSerializer
 from .permissions import IsAuthorOrReadOnly
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+@api_view(['POST'])
+def postCreate(request):
+    serializer = ArticleSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
 
 class ArticleAPIView(generics.ListAPIView):
     permission_classes = (IsAuthorOrReadOnly,) 
